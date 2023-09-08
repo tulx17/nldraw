8b9a1812bc5e2eb64f3822885d5a7a024ec58e51
Le X. Tu <lexuantu1002@gmail.com>
2023-09-08 17:36:33 +0700

    remove web specific features (visually)

--- src/pages/draw/Draw.tsx
+++ src/pages/draw/Draw.tsx
@@ -2,7 +2,12 @@ import { Canvas } from "@/components";
 import { EMPTY_STRING, SPACE_SEPARATOR } from "@/constants/primitive";
 import { useInit } from "@/hooks";
 import { loadSnapshot, saveSnapshot } from "@/pages/draw/Draw.module";
-import { TLStore, createTLStore, defaultShapeUtils } from "@tldraw/tldraw";
+import {
+  TLStore,
+  TLUiMenuGroup,
+  createTLStore,
+  defaultShapeUtils,
+} from "@tldraw/tldraw";
 import { Toast } from "antd-mobile";
 import { Fragment, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
@@ -58,7 +63,77 @@ export function Draw() {
 
   return (
     <Fragment>
-      <Canvas store={store} />
+      <Canvas
+        store={store}
+        overrides={{
+          helpMenu(_editor, schema, _helpers) {
+            const top = schema.find(
+              (item) => item.id === "top"
+            )! as TLUiMenuGroup;
+
+            // remove keyboard shortcuts
+            const topKeyboardShortcutsIndex = top.children.findIndex(
+              (item) => item.id === "keyboard-shortcuts"
+            );
+            top.children.splice(topKeyboardShortcutsIndex, 1);
+
+            return schema;
+          },
+          actions(_editor, actions, _helpers) {
+            return actions;
+          },
+          contextMenu(_editor, schema, _helpers) {
+            // remove clipboard options
+            const clipboardGroup = schema.findIndex(
+              (item) => item.id === "clipboard-group"
+            );
+            schema.splice(clipboardGroup, 1);
+
+            // remove conversion options
+            const conversions = schema.findIndex(
+              (item) => item.id === "conversions"
+            );
+            schema.splice(conversions, 1);
+
+            return schema;
+          },
+          menu(_editor, schema, _helpers) {
+            const menu = schema.find(
+              (item) => item.id === "menu" && item.type === "group"
+            )! as TLUiMenuGroup;
+
+            //  remov file options (print)
+            const menuFileIndex = menu.children.findIndex(
+              (item) => item.id === "file"
+            );
+            menu.children.splice(menuFileIndex, 1);
+
+            const menuEdit = menu.children.find(
+              (item) => item.id === "edit"
+            )! as TLUiMenuGroup;
+
+            // remove conversion options (copy,paste,export)
+            const editConversionsIndex = menuEdit.children.findIndex(
+              (item) => item.id === "conversions"
+            );
+            menuEdit.children.splice(editConversionsIndex, 1);
+
+            // remove clipboard options (cut,copy,paste)
+            const editClipboardIndex = menuEdit.children.findIndex(
+              (item) => item.id === "clipboard-actions"
+            );
+            menuEdit.children.splice(editClipboardIndex, 1);
+
+            // remove extras options (insert embed and media)
+            const extrasIndex = schema.findIndex(
+              (item) => item.id === "extras"
+            );
+            schema.splice(extrasIndex, 1);
+
+            return schema;
+          },
+        }}
+      />
     </Fragment>
   );
 }
